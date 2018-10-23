1a88ab12101b098f8ee1d86210b9b4851d3ef389
DennySPB <dennyspb@gmail.com>
2018-10-23 14:58:12 +0300

    Revert "tinycompress: Generate vendor specifc tinycompress"
    Allow use normal tinycompress for CAF audio HALs
    
    This reverts commit 026d01ca638367c2acaf8191dc97060ee7c52305.
    
    Change-Id: I326298522d4552ff9bab6c904052c17ab3acb760
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- compress.c
+++ compress.c
@@ -572,8 +572,6 @@ int compress_set_gapless_metadata(struct compress *compress,
 	return 0;
 }
 
-#ifdef USE_VENDOR_EXTN
-#if defined(SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM)
 int compress_set_next_track_param(struct compress *compress,
 	union snd_codec_options *codec_options)
 {
@@ -584,14 +582,6 @@ int compress_set_next_track_param(struct compress *compress,
 		return oops(compress, errno, "cannot set next track params\n");
 	return 0;
 }
-#else
-int compress_set_next_track_param(struct compress *compress __unused,
-	union snd_codec_options *codec_options __unused)
-{
-	return 0;
-}
-#endif
-#endif
 
 bool is_codec_supported(unsigned int card, unsigned int device,
 		unsigned int flags, struct snd_codec *codec)
